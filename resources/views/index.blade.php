003b936e795d696f48c073849c99ef15d60f6210
MyNameIsAlpaca <Saragosagabriele@gmail.com>
2023-05-08 13:30:12 +0200

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="It">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Prova Laravel</title>
+</head>
+<body>
+  <h1>Hello World!</h1>
+</body>
+</html>
